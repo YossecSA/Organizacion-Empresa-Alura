9c1646c3bc2265fb6af65440d021d5a89738bd5d
YossecSA <yosec.arratea22@gmail.com>
2024-11-30 13:02:04 -0500

    funcionamiento completo, del formulario, tener en cuenta q siempre el padre es quien pasa informacion al hijo y no al revez

--- src/components/CampoTexto/CampoTexto.jsx
+++ src/components/CampoTexto/CampoTexto.jsx
@@ -2,10 +2,19 @@ import React from "react";
 import "./CampoTexto.css";
 
 const CampoTexto = (props) => {
+    const manejarCambio = (e) => {
+        props.actualizarValor(e.target.value);
+    };
+
     return (
         <div className="campo-texto">
             <label>{props.titulo}</label>
-            <input placeholder={props.placeholder} required={props.required}/>
+            <input
+                placeholder={props.placeholder}
+                required={props.required}
+                value={props.valor}
+                onChange={manejarCambio}
+            />
         </div>
     );
 };
--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -1,25 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Formulario.css";
 import CampoTexto from "../CampoTexto/CampoTexto";
 import ListaOpciones from "../ListaOpciones/ListaOpciones";
 import Boton from "../Boton/Boton";
 
 const Formulario = () => {
+    const [nombre, setNombre] = useState(second);
+    const [puesto, setPuesto] = useState(second);
+    const [foto, setFoto] = useState(second);
 
     const manejarEnvio = (e) => {
         e.preventDefault();
-        console.log(e)
-    }
+
+        let datosEnviar = {
+            nombre,
+            puesto,
+            foto,
+        };
+    };
 
     return (
         <section className="formulario">
             <form onSubmit={manejarEnvio}>
                 <h2>Rellena el formulario para crear el colaborador.</h2>
-                <CampoTexto titulo="Nombre" placeholder="Ingresar Nombre" required />
-                <CampoTexto titulo="Puesto" placeholder="Ingresar Puesto" required />
-                <CampoTexto titulo="Foto" placeholder="Ingresar Foto" required />
+                <CampoTexto
+                    titulo="Nombre"
+                    placeholder="Ingresar Nombre"
+                    required
+                    valor={nombre}
+                    actualizarValor={setNombre}
+                />
+                <CampoTexto
+                    titulo="Puesto"
+                    placeholder="Ingresar Puesto"
+                    required
+                    valor={puesto}
+                    actualizarValor={setPuesto}
+                />
+                <CampoTexto
+                    titulo="Foto"
+                    placeholder="Ingresar Foto"
+                    required
+                    valor={foto}
+                    actualizarValor={setFoto}
+                />
                 <ListaOpciones />
-                <Boton texto='Crear'/>
+                <Boton texto="Crear" />
             </form>
         </section>
     );
