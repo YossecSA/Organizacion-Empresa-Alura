f168739af365a69089c74a72afe639750062b115
YossecSA <yosec.arratea22@gmail.com>
2024-11-30 11:04:06 -0500

    creacion del header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.css"
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="/img/header.png" alt="Header de la imagen" />
+        </header>
+    );
+};
+
+export default Header;
