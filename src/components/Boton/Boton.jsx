d06be675078a82aab7837352a7517fc1f4dccd28
YossecSA <yosec.arratea22@gmail.com>
2024-11-30 12:20:33 -0500

    creacion del boton

---
+++ src/components/Boton/Boton.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Boton.css"
+
+const Boton = (props) => {
+  return (
+    <button>{props.texto}</button>
+  )
+}
+
+export default Boton
--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Formulario.css";
 import CampoTexto from "../CampoTexto/CampoTexto";
 import ListaOpciones from "../ListaOpciones/ListaOpciones";
+import Boton from "../Boton/Boton";
 
 const Formulario = () => {
     return (
@@ -12,6 +13,7 @@ const Formulario = () => {
                 <CampoTexto titulo="Puesto" placeholder="Ingresar Puesto"/>
                 <CampoTexto titulo="Foto" placeholder="Ingresar Foto"/>
                 <ListaOpciones />
+                <Boton texto='Crear'/>
             </form>
         </section>
     );
